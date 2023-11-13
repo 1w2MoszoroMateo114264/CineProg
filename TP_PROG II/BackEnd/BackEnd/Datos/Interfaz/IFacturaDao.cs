d84562c69da241f441cd98b474a10e663d073608
1w2MoszoroMateo114264 <114264@tecnicatura.frc.utn.edu.ar>
2023-11-13 17:15:37 -0300

    lo mismo de antes

--- TP_PROG II/BackEnd/BackEnd/Datos/Interfaz/IFacturaDao.cs
+++ TP_PROG II/BackEnd/BackEnd/Datos/Interfaz/IFacturaDao.cs
@@ -10,9 +10,16 @@ namespace BackEnd.Datos.Interfaz
     public interface IFacturaDao
     {
         List<Funciones> GetFunciones();
+        List<Edades> GetEdades();
+        List<Forma_de_pagos> GetFormasPago();
+        List<Genero_Pelis> GetGenero_Pelis();
+        List<Peliculas> GetPeliculas();
+        List<Salas> GetSalas();
+        List<Sucursales> GetSucursales();
+        List<Tipo_Entrada> GetTipoEntradas();
         bool crearFactura(Factura oFactura);
         bool actualizarFactura(Factura oFactura);
         List<Factura> ObtenerFacturaPorFiltros(DateTime desde, DateTime hasta, int dni);
-
+        
     }
 }
